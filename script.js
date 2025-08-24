f13b5a4c90e1a3e6077f4b27a07b419776c71e31
Gowri009 <gowrias2004@gmail.com>
2025-08-24 10:15:10 +0530

    p

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init({ duration: 1000 });
+
+function openModal() {
+  document.getElementById("eduModal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("eduModal").classList.add("hidden");
+}
